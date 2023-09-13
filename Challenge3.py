ca47eac16a74541ed979ec2fbcfdb0cceddc1f5b
Ilyas AKIOUI <129302570+Ilyas-00@users.noreply.github.com>
2023-09-13 23:23:30 +0200

    Update Challenge3.py

--- Challenge3.py
+++ Challenge3.py
@@ -1,7 +1,6 @@
 # Given a valid (IPv4) IP address, return a defanged version of that IP address.
 
 # A defanged IP address replaces every period "." with "[.]".
-
 # Example 1:
 
 # Input: address = "1.1.1.1"
